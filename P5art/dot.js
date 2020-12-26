045e0d9b47e5c3cedd2c39a71b4bfc625849718a
drsrvdveen <drs.r.vd.veen@gmail.com>
2020-12-26 18:42:34 +0000

    dotFoto

--- P5art/dot.js
+++ P5art/dot.js
@@ -1,25 +1,78 @@
+function preload() {
+  foto = loadImage("dotsourceP.jpg");
+}
+
 function setup() {
-    canvas = createCanvas(1000,450);
+    canvas = createCanvas(256,256);
     canvas.parent('processing');
     colorMode(RGB,255,255,255,1);
     noStroke();
     noLoop();
+    foto.loadPixels();
 }
 
-var D = 200;
-var R = D/2;
-var N = 200;
+/* testsettings
+var D = 50;
+var N = 10;
 var rndF = 5;
-
+var dotSpreiding = 3;
+var maxAlfa = 10;
+// einde testsettings
+*/
+var D = 12;
+var N = 3;
+var rndF = 5;
+var dotSpreiding = 5;
+var maxAlfa = 10;
+var modulus = 5;
 
 function draw() {
-    background(250);
-    fill(255,0,255,1/N);
+    background(245);
+    foto.resize(256,256);
+    toonFoto(modulus,true);
+    /*
+    fill(255,0,255,0.01*maxAlfa/N);
     translate(width/2,height/2);
-    for (var n=0;n<N;n++) {
+    samengesteldeDot(N,dotSpreiding,false);
+    */
+    //background(foto);
+}
+
+function toonFoto(onderlingeAfstand,bronTonen) {
+    var trial = 256;
+    for (var k = 0;k < foto.width;k++) {
+        for (var r = 0;r < foto.height;r++) {
+            if (k % onderlingeAfstand == 0 && r % onderlingeAfstand == 0) {
+                push();
+                pixelKleur = foto.get(k,r);
+                fill(pixelKleur[0],pixelKleur[1],pixelKleur[2],0.01*maxAlfa/N);
+                translate(k,r);
+                samengesteldeDot(N,dotSpreiding);
+                if (bronTonen) {
+                    stroke(pixelKleur);
+                    point(0,0);
+                }
+                pop();
+            }
+        }
+    }
+}
+
+function samengesteldeDot(diepte,spreiding) {
+    for (var d=0;d<diepte;d++) {
         push();
-        translate(random(-D/rndF,D/rndF),random(-D/rndF,D/rndF));
-        ellipse(0,0,D);
+        var v = spreiding;
+        translate(random(-v,v),random(-v,v));
+        tekenDot(D,N,rndF);
+        pop();
+    }
+}
+
+function tekenDot(diameter,aantalLagen,inperking,percentageVulling) {
+    for (var n=0;n<aantalLagen;n++) {
+        push();
+        translate(random(-diameter/inperking,diameter/inperking),random(-diameter/inperking,diameter/inperking));
+        ellipse(0,0,diameter);
         pop();
     }
 }
